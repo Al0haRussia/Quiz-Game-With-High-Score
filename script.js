4b2695250bdab48ff1ed4b72d8c368855d69ff27
Kyle G <legokyle@frontier.com>
2020-11-27 13:13:09 -0800

    Added css style sheet, timer and button fore highscores

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-var heading = document.querySelector("#heading")
-heading.textContent = "Test";
-heading.style = "color: blue";
+var highscore = document.querySelector("#highscore");
+highscore.textContent = "View Highscore";
+highscore.style = "font-weight: bold";
+
+
+var secondsLeft = 60;
+var time = document.querySelector("#timer");
+
+function prepareRead() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    time.textContent = "Time: " + secondsLeft;    
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+prepareRead();
